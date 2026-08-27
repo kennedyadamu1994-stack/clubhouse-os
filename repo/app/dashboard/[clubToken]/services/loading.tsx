4d08fa70167f41488aa02d65f92b1f1fa5744125
kennedyadamu1994-stack <kennedyadamu1994@gmail.com>
2026-08-27 18:35:17 +0100

    delete

--- repo/app/dashboard/[clubToken]/services/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { CardGridSkeleton } from "@/components/skeletons/card-grid-skeleton";
-
-export default function Loading() {
-  return <CardGridSkeleton gridClassName="service-cardgrid" count={4} />;
-}
