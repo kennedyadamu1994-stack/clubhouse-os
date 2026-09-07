6ed897e71adcb4b7d2ceb9fd72f14348541869dc
kennedyadamu1994-stack <kennedyadamu1994@gmail.com>
2026-09-07 22:37:44 +0100

    Delete repo directory

--- repo/app/dashboard/[clubToken]/workspace/perks/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { CardGridSkeleton } from "@/components/skeletons/card-grid-skeleton";
-
-export default function Loading() {
-  return <CardGridSkeleton gridClassName="perk-cardgrid" count={4} />;
-}
